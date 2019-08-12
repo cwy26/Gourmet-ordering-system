1c8f914401ceab8606d36493d25129cc25a87be4
Harvey <52202752+cwy26@users.noreply.github.com>
2019-08-12 12:03:36 +0800

    完成预约页面的左右联动功能

--- gm-ordersystem/src/router.js
+++ gm-ordersystem/src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloContainer from "./components/HelloWorld.vue"
 import index from './views/xds_index.vue'
-
+import Reservation from './views/Reservation.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -12,5 +12,6 @@ export default new Router({
       component: HelloContainer
     },
     {path: '/index',component: index},
+    {path: '/reservation',component: Reservation}
   ]
 })
